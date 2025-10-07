d83e4a9d5841b325aa9397e832f591b30de0ec92
Pedro Laracuente <laracuentepedro@gmail.com>
2025-10-07 08:41:16 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+def main():
+    print("Hello from ai-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    if len(sys.argv) < 2:
+        print("Error: Please provide a prompt")
+        sys.exit(1)
+    user_prompt = sys.argv[1]
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=user_prompt
+        )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+
+if __name__ == "__main__":
+    main()
