29597d3f5c3096ffe2fb19fb57587f5c6f3c5910
Pedro Laracuente <laracuentepedro@gmail.com>
2025-10-11 09:59:34 -0400

    feat: add agent loop with function calling feedback refactor: streamline response handling and improve function call processing

--- main.py
+++ main.py
@@ -33,37 +33,40 @@ def main():
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            system_instruction=system_prompt,
-            tools=[available_functions]
-            ),
-        
-        )
-    if verbose:
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-
-    if not response.function_calls:
-        print(response.text)
-
-    function_responses = []
-    for function_call_part in response.function_calls:
-        function_call_result = call_function(function_call_part, verbose)
-        if (
-            not function_call_result.parts[0].function_response.response
-            or not function_call_result.parts
-            ):
-            raise Exception("Error: No function call")
+    for _ in range(20):        
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                system_instruction=system_prompt,
+                tools=[available_functions]
+                ),
+            )
+        for candidate in response.candidates:
+            messages.append(candidate.content)
         if verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
-            function_responses.append(function_call_result.parts[0])
-        if not function_responses:
-            raise Exception("no function responses generated, exiting.")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
 
-        print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+        if not response.function_calls:
+            print(response.text)
+
+        function_responses = []
+        for function_call_part in response.function_calls:
+            function_call_result = call_function(function_call_part, verbose)
+            if (
+                not function_call_result.parts[0].function_response.response
+                or not function_call_result.parts
+                ):
+                raise Exception("Error: No function call")
+            if verbose:
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+                function_responses.append(function_call_result.parts[0])
+            if not function_responses:
+                raise Exception("no function responses generated, exiting.")
+            tool_msg = types.Content(role="user", parts=function_responses)
+            messages.append(tool_msg)
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
             
